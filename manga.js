a0b65fcf35379facba5cbb9ed16fba1a03393f35
Tan Jun Hui <junhui.tan.2017@sis.smu.edu.sg>
2021-07-30 17:55:37 +0800

    Add comments

--- manga.js
+++ manga.js
@@ -1,24 +1,31 @@
 // Functions for changing pages
 const pageUtils = (function () {
-    let currPage = 2;
-    let latestPage = 100;
+    let currPage = 2;       // The middle page of current set of page eg. [2, [3], 4] or [1, 2, [3], 4, 5]
+    let latestPage = 100;   // The latest page from xkcd
+
+    // Updating latest page number
     fetch("https://xkcd.vercel.app/?comic=latest").then((res) => {
         res.json().then((result) => {
             latestPage = result.num;
         });
     });
+
     return {
         getPage: () => currPage,
         getLatest: () => latestPage,
         changePage: (numPageChange) => {
+            // To flip the set of pages
             currPage += numPageChange;
             if (currPage < 1) {
+                // If page number goes below 1, loop back to latest page number
                 currPage += latestPage;
             } else if (currPage > latestPage) {
+                // If page number exceeds latest page num, loop back to 1
                 currPage -= latestPage;
             }
         },
         setPage: (page) => {
+            // To set page number when navigating to specific page
             currPage = page;
         }
     }
@@ -26,7 +33,7 @@ const pageUtils = (function () {
 
 // Functions for changing display
 const mainFunctions = (function () {
-    let numDisplay = 3;
+    let numDisplay = 3; // The number of pages to display at once
 
     return {
         // Set the number of comic display
@@ -36,11 +43,17 @@ const mainFunctions = (function () {
             const midPage = pageUtils.getPage();
             const lastPage = pageUtils.getLatest();
 
-            const mainDisplayElement = document.querySelector("#main-display");
-            mainDisplayElement.style.gridTemplateColumns = `${90 / num}% `.repeat(num);
+            const mainDisplayElement = document.querySelector("#main-display");         // Div element that contains the comics
+            mainDisplayElement.style.gridTemplateColumns = `${90 / num}% `.repeat(num); // To space the comics evenly within the div
+            // Create the individual divs for the comic content first
             mainDisplayElement.innerHTML = Array(num).fill(0).map((val, idx) => `<div id="manga${idx + 1}"></div>`).join("");
-            Array(num).fill(0).forEach((val, i) => {
-                const idx = i + 1;
+            // Determine the page number for each comic
+            for (let idx = 1; idx <= num; idx++) {
+                /* Eg. midPage = 1000, numDisplay = 3
+                 * leftPage = 1000 + 1 - Math.ceil(3/2) = 999
+                 * midPage = 1000 + 2 - Math.ceil(3/2) = 1000
+                 * rightPage = 1000 + 3 - Math.ceil(3/2) = 1001
+                 */
                 let pageNo = midPage + idx - (Math.ceil(num / 2));
                 if (pageNo < 1) {
                     pageNo = lastPage + pageNo;
@@ -48,7 +61,7 @@ const mainFunctions = (function () {
                     pageNo = pageNo - lastPage;
                 }
                 displayManga(`manga${idx}`, pageNo);
-            });
+            };
         },
         // When previous button is clicked
         prevClick: () => {
@@ -65,28 +78,36 @@ const mainFunctions = (function () {
             pageUtils.setPage(Math.floor(Math.random() * pageUtils.getLatest()));
             mainFunctions.setDisplay(numDisplay);
         },
+        // To determine if a particular number of display is being selected
         isSelected: (selected) => {
             return selected === numDisplay;
         },
         // When a preference is submitted (Specific page or number of display)
         goClick: () => {
-            let selectedPage = document.querySelector("#select-page").value;
+            let selectedPage = document.querySelector("#select-page").value; // The page number that the user inputted
             if (selectedPage != "") {
-                selectedPage = parseInt(selectedPage);
+                selectedPage = parseInt(selectedPage); // Convert user input into type number
                 if (selectedPage < 1 || selectedPage > pageUtils.getLatest()) {
+                    // When user input page that is out of range
                     alert(`Please select a number between 1 and ${pageUtils.getLatest()}`);
                 } else {
+                    // Update the UI
                     pageUtils.setPage(selectedPage);
                     mainFunctions.setDisplay(numDisplay);
                 }
             }
 
-            const selectedDisplay = parseInt(document.querySelector("#display-dropdown").value);
+            const selectedDisplay = parseInt(document.querySelector("#display-dropdown").value); // The selected option of the dropdown
+            // Only update when the number of display is changed
             if (selectedDisplay != numDisplay) {
+                // Eg. When the current page displayed is [1, 2, 3], then user change to displaying 5
+                // The page will display [1, 2, 3, 4, 5] instead of [latest page, 1, 2, 3, 4]
                 if (pageUtils.getPage() < selectedDisplay / 2) {
                     pageUtils.setPage(Math.ceil(selectedDisplay / 2));
                 }
+                // Update the UI
                 mainFunctions.setDisplay(selectedDisplay);
+                // Update the dropdown option
                 document.querySelector("#display-dropdown").innerHTML = `
                 <option value="1" ${mainFunctions.isSelected(1) ? "selected" : ""}>1</option>
                 <option value="3" ${mainFunctions.isSelected(3) ? "selected" : ""}>3</option>
@@ -123,14 +144,17 @@ Example Result
 function displayManga(id, page) {
     fetch(`https://xkcd.vercel.app/?comic=${page}`).then((res) => {
         res.json().then((result) => {
-            const comicDivParts = document.querySelector(`#${id}`).children;
+            // Update the values of the specific div holding the comic
+            const comicDivParts = document.querySelector(`#${id}`).children; // Div that holds the comic content
             comicDivParts[0].innerHTML = result.safe_title;
             comicDivParts[1].id = `manga-${result.num}`;
             comicDivParts[1].src = result.img;
             comicDivParts[1].addEventListener('click', () => {imgClick(result.img)});
             comicDivParts[2].innerHTML = result.num;
         })
-    })
+    });
+
+    // Display loading gif first while fetching the API
     document.querySelector(`#${id}`).innerHTML = `
         <h1>Loading...</h1>
         <img src="https://cdn.dribbble.com/users/712682/screenshots/11956378/media/50c8e606db69f492200555d72b34f308.gif"/>
